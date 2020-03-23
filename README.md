af339cff77be27f8b9b2c355239149ae6a895018
Cate Dunn <60521577+CateDunn@users.noreply.github.com>
2020-03-22 22:06:34 -0400

    functioning text input boxes and saving to local storage

--- README.md
+++ README.md
@@ -24,18 +24,22 @@ THEN the current day is displayed at the top of the calendar
 
 WHEN I scroll down
 THEN I am presented with timeblocks for standard business hours
+    yes
 
 WHEN I view the timeblocks for that day
 THEN each timeblock is color coded to indicate whether it is in the past, present, or future
 
 WHEN I click into a timeblock
 THEN I can enter an event
+    yes
 
 WHEN I click the save button for that timeblock
 THEN the text for that event is saved in local storage
+    yes
 
 WHEN I refresh the page
 THEN the saved events persist
+    yes
 ```
 
 The following animation demonstrates the application functionality:
--- script.js
+++ script.js
@@ -9,3 +9,59 @@ console.log(dateResult);
 $("#currentDay").html(dateResult);
 
 console.log(moment.format(format))
+
+
+
+var btn = document.getElementsByClassName('btn');
+console.log(btn);
+
+function myFunction9(){
+    var str = document.getElementById('text9').value;
+    console.log(str)
+    $("#9AM").text(str);
+    localStorage.setItem('9AM', str);
+};
+
+function myFunction10(){
+    var str = document.getElementById('text10').value;
+    $("#10AM").text(str);
+};
+
+function myFunction11(){
+    var str = document.getElementById('text11').value;
+    $("#11AM").text(str);
+};
+function myFunction12(){
+    var str = document.getElementById('text12').value;
+    $("#12PM").text(str);
+};
+function myFunction1(){
+    var str = document.getElementById('text1').value;
+    $("#1PM").text(str);
+};
+function myFunction2(){
+    var str = document.getElementById('text2').value;
+    $("#2PM").text(str);
+};
+function myFunction3(){
+    var str = document.getElementById('text3').value;
+    $("#3PM").text(str);
+};
+function myFunction4(){
+    var str = document.getElementById('text4').value;
+    $("#4PM").text(str);
+};
+function myFunction5(){
+    var str = document.getElementById('text5').value;
+    $("#5PM").text(str);
+};
+
+function renderLast(){
+    var event = localStorage.getItem("9AM");
+    if (event === null) {
+    return;
+  };
+  $("#9AM").text(event);
+}
+
+renderLast()
